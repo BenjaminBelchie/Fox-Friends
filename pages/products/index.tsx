ca242366ae5ec8f4982a5d81a238ae821304fec2
BenjaminBelchie <benbelcher112@gmail.com>
2023-08-06 19:28:01 +0100

    Added the products galary with test data

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,3 +1,32 @@
-export default function ProductsPage() {
-  return <></>;
+import Gallary from '../../components/Gallary';
+import { Product } from '../../types/Product';
+
+export async function getStaticProps() {
+  const testData: Product[] = [];
+
+  for (let i = 0; i < 12; i++) {
+    testData.push({
+      id: i,
+      image: '/test.png',
+      link: 'https://www.etsy.com/uk',
+      name: 'Test product',
+      price: '50',
+      shortDescription:
+        'This is a test product to show the elements on screen.',
+      longDescription: 'This is a test product',
+    });
+  }
+  return {
+    props: {
+      data: testData,
+    },
+  };
+}
+
+export default function ProductsPage({ data }) {
+  return (
+    <>
+      <Gallary data={data} />
+    </>
+  );
 }
