e291ae646aaddb092645ea40a4585625ca566174
Ben Belcher <75249728+BenjaminBelchie@users.noreply.github.com>
2023-08-06 17:02:58 +0100

    Updated the favcion and title (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>The Coffee House</title>
+        <title>Anna's Crochet</title>
       </Head>
       <main className={`${montserrat.variable} font-sans`}>
         <Component {...pageProps} />
