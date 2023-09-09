984e2ae1aa2b082a9750a998352a76cfb9cffa21
Ben Belcher <75249728+BenjaminBelchie@users.noreply.github.com>
2023-09-09 21:23:43 +0100

    Connected to the admin API's to fetch product and site data (#24)
    * Connected to the admin API's to fetch product and site data
    
    * Updated build script to generate prisma client

--- components/Gallary.tsx
+++ components/Gallary.tsx
@@ -1,6 +1,12 @@
+import { Product } from '@prisma/client';
 import ProductCard from './ProductCard';
+import { FlatProductsWithTagsAndImages } from '../util/createFlatProductObject';
 
-export default function Gallary({ data }) {
+type Props = {
+  data: FlatProductsWithTagsAndImages[];
+};
+
+export default function Gallary({ data }: Props) {
   return (
     <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,21 +1,37 @@
 import Image from 'next/image';
 import { Product } from '../../types/Product';
+import { prisma } from '../../prisma/prisma';
+import {
+  FlatProductsWithTagsAndImages,
+  createFlatProductsObject,
+} from '../../util/createFlatProductObject';
+import { supabaseProductImagePrefix } from '../../util/imagePrefix';
+import EmblaCarousel from '../../components/Carousel';
+import { EmblaOptionsType } from 'embla-carousel-react';
 
-export default function Product({ product }) {
+type Props = {
+  product: FlatProductsWithTagsAndImages;
+};
+
+export default function Product({ product }: Props) {
+  const OPTIONS: EmblaOptionsType = {};
+  const SLIDE_COUNT = product.images.length;
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
   return (
     <div className="flex h-screen flex-col justify-between">
-      <div className="mx-auto mt-16 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="mx-auto flex flex-col sm:flex-row">
-          <Image
-            alt="coffee"
-            className="rounded-lg"
-            src={product.image}
-            width={560}
-            height={640}
-          />
-          <div className="mt-10 flex flex-col sm:mt-0 sm:ml-10">
+      <div className="mx-auto sm:mt-16 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto flex flex-col-reverse sm:flex-row">
+          <div className="flex-initial sm:w-1/2">
+            <EmblaCarousel
+              slides={SLIDES}
+              options={OPTIONS}
+              images={product.images.map(img => img.image)}
+            />
+          </div>
+
+          <div className="mt-10 flex-initial sm:w-1/2 flex-col sm:mt-0 sm:ml-10">
             <h1 className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">
-              {product.name}
+              {product.title}
             </h1>
             <h1 className="mt-3 text-4xl font-bold text-gray-500 sm:text-3xl sm:tracking-tight lg:text-3xl">
               £{product.price}
@@ -31,28 +47,15 @@ export default function Product({ product }) {
   );
 }
 
-export async function getStaticProps({ params }) {
-  const product: Product = {
-    id: 1,
-    image: '/test.png',
-    link: 'https://www.etsy.com/uk',
-    name: 'Test product',
-    price: '50',
-    shortDescription: 'This is a test product to show the elements on screen.',
-    longDescription: `Introducing our adorable Handmade Crochet Cow - the perfect cuddly companion for children and animal lovers of all ages! Crafted with love and care, this unique and whimsical creation is sure to capture hearts and bring smiles to faces. Each crochet cow is meticulously handcrafted by skilled artisans using soft and premium quality yarn, ensuring a plush and huggable texture that is gentle to the touch. The attention to detail is evident in every stitch, from the charming embroidered eyes to the cute little tail, making it a truly one-of-a-kind piece.`,
-  };
+export async function getServerSideProps({ params }) {
+  const product = await prisma.product.findUnique({
+    where: { id: params.slug },
+    include: { tags: { include: { tag: true } }, images: true },
+  });
+  const flatProduct = createFlatProductsObject(product);
   return {
     props: {
-      product: product,
+      product: flatProduct,
     },
-  };
-}
-
-export async function getStaticPaths() {
-  let fullPaths = [];
-
-  return {
-    paths: fullPaths,
-    fallback: 'blocking',
   };
 }
